b3e279bc6874c95a62a7cda9a1061f2708538552
aidenwvt <142836917+aidenwvt@users.noreply.github.com>
2025-10-22 02:49:52 -0400

    Delete server directory

--- server/__main__.py
+++
@@ -1,9 +0,0 @@
-import threading
-from .gui import guiMain
-from .server import serverMain
-
-if __name__ == "__main__":
-    server_thread = threading.Thread(target=serverMain, daemon=True)
-    server_thread.start()
-
-    guiMain()
